afe902f8b83ca3f6533d65a570d128d3682cf22a
Shivam Sharma <kshivam654@gmail.com>
2020-10-17 14:32:38 +0530

    cors added to comment service

--- comments/index.js
+++ comments/index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-Parser");
+const cors = require("cors");
 
 const { randomBytes } = require("crypto");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get("/posts/:id/comments", (req, res) => {
-    res.send(commentsByPostId[req.params.id] || [])
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/posts/:id/comments", (req, res) => {
     const commentsId = randomBytes(4).toString("hex");
-    const {content} = req.body;
+    const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({ id: commentsId, content });
